74491d64a4c95f1fc821305796c38bcce949f3b6
Mansour Emami <mansuurem@gmail.com>
2025-12-29 19:35:20 +0100

    background color

---
+++ 12.1 DOM/index.js
@@ -0,0 +1,12 @@
+alert("hi");
+for(let i=0;i<10;i++){
+    console.log(i);
+}
+console.log("Hello World");
+
+document.firstElementChild.lastElementChild.querySelector("ul").lastElementChild.innerHTML= "Angela";
+document.getElementsByTagName("li")[2].style.color="purple";
+document.querySelector("ul .list").style.color="green"; 
+document.querySelectorAll("ul .list")[0].style.color="orange";
+document.querySelector("ul li a").style.color="red";
+document.querySelector("button").style.backgroundColor="yellow";
--- DOM/index.js
+++
@@ -1,5 +0,0 @@
-alert("hi");
-for(let i=0;i<10;i++){
-    console.log(i);
-}
-console.log("Hello World");
