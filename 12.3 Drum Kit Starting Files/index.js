2d721dbe263e512208e18beec7cb9508b64c477b
Mansour Emami <mansuurem@gmail.com>
2026-01-02 19:58:18 +0100

    Animation function an keys

--- 12.3 Drum Kit Starting Files/index.js
+++ 12.3 Drum Kit Starting Files/index.js
@@ -1,26 +1,46 @@
+//detecting button press 
 for(var i=0; i<document.querySelectorAll(".drum").length; i++){
     document.querySelectorAll(".drum")[i].addEventListener("click", function(){
-
-        switch(this.innerHTML){
-
-            case "w": var audio = new Audio("./sounds/tom-1.mp3");audio.play();
-            break;
-            case "a": var audio = new Audio("./sounds/tom-2.mp3");audio.play();
-            break;
-            case "s": var audio = new Audio("./sounds/tom-3.mp3");audio.play();
-            break;
-            case "d": var audio = new Audio("./sounds/tom-4.mp3");audio.play();
-            break;
-            case "j": var audio = new Audio("./sounds/snare.mp3");audio.play();
-            break;
-            case "k": var audio = new Audio("./sounds/crash.mp3");audio.play();
-            break;
-            case "l": var audio = new Audio("./sounds/kick-bass.mp3");audio.play();
-            break;
-            default: alert("false button");
-            break;
-
-
-        }
+        var buttonInnerHTML = this.innerHTML;
+        makeSound(buttonInnerHTML);
+        buttonAnimation(buttonInnerHTML);
     });
+}
+
+// detecting keybourd press
+document.addEventListener("keydown", function(event){
+    makeSound(event.key);
+    buttonAnimation(event.key);
+});
+
+
+function makeSound(key){
+     switch (key) {
+        case "w": var audio = new Audio("./sounds/tom-1.mp3");audio.play();
+        break;
+        case "a": var audio = new Audio("./sounds/tom-2.mp3");audio.play();
+        break;
+        case "s": var audio = new Audio("./sounds/tom-3.mp3");audio.play(); 
+        break;
+        case "d": var audio = new Audio("./sounds/tom-4.mp3");audio.play();
+        break;
+        case "j": var audio = new Audio("./sounds/snare.mp3");audio.play();
+        break;
+        case "k": var audio = new Audio("./sounds/crash.mp3");audio.play();
+        break;
+        case "l": var audio = new Audio("./sounds/kick-bass.mp3");audio.play();
+        break;
+        default: console.log("problem...");
+        break;
+    }
+}
+
+function buttonAnimation(currentKey){
+
+    var activeButton = document.querySelector("."+currentKey);
+    activeButton.classList.add("pressed");
+
+    setTimeout(function(){
+        activeButton.classList.remove("pressed");
+    }, 100);
 }
